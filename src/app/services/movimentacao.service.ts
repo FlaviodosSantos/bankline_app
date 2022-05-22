2f16eeb952adf1e5740bd28ebad3cacee323a227
Flavio dos Santos <flaviovorthrox@yahoo.com.br>
2022-05-21 22:23:06 -0300

    movimentação por correntista __ 1h50

--- src/app/services/movimentacao.service.ts
+++ src/app/services/movimentacao.service.ts
@@ -14,6 +14,10 @@ export class MovimentacaoService {
     return this.http.get(`${baseUrl}/movimentacoes`);
   }
 
+  findByIdConta(idConta: any): Observable<any> {
+    return this.http.get(`${baseUrl}/movimentacoes/${idConta}`);
+  }
+
   // adicionando o método de inclusão (POST) via API
   create(movimentacao: any): Observable<any> {
     return this.http.post(`${baseUrl}/movimentacoes`, movimentacao);
